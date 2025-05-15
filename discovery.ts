b42b3f166be924a50ba817e46e2b066cd13df005
DaughterOfZaun <jinx52538@gmail.com>
2025-05-15 08:25:06 +0300

    untested torrent-discovery

--- discovery.ts
+++ discovery.ts
@@ -1,11 +1,18 @@
-const Discovery = await import('torrent-discovery'!)
-
 import { TypedEventEmitter, peerDiscoverySymbol, serviceCapabilities } from '@libp2p/interface'
-import type { ComponentLogger, Logger, PeerDiscovery, PeerDiscoveryEvents, PeerDiscoveryProvider, PeerId, PeerInfo, Startable } from '@libp2p/interface'
+import type { ComponentLogger, Libp2pEvents, Logger, PeerDiscovery, PeerDiscoveryEvents, PeerDiscoveryProvider, PeerId, PeerInfo, Startable, TypedEventTarget } from '@libp2p/interface'
 import { multiaddr, type Multiaddr } from '@multiformats/multiaddr'
 import { identity } from 'multiformats/hashes/identity'
 import { peerIdFromMultihash } from '@libp2p/peer-id'
 import { text2arr } from 'uint8-util'
+import type { ConnectionManager } from '@libp2p/interface-internal'
+import { ipPortToMultiaddr } from '@libp2p/utils/ip-port-to-multiaddr'
+//@ts-ignore
+import addrToIPPort from 'addr-to-ip-port'
+//@ts-ignore
+import Discovery from 'torrent-discovery'
+
+import { version as VERSION } from 'webtorrent/package.json'
+const USER_AGENT = `WebTorrent/${VERSION} (https://webtorrent.io)`
 
 interface DiscoveryInit {
     infoHash: string,
@@ -13,51 +20,70 @@ interface DiscoveryInit {
     announce: string[],
     dht: boolean | any,
     dhtPort: number,
-    userAgent: string,
     tracker: boolean | any,
     lsd: boolean | any,
 }
 interface DiscoveryComponents {
     peerId: PeerId
     logger: ComponentLogger
+    connectionManager: ConnectionManager
+    events: TypedEventTarget<Libp2pEvents>
+}
+
+export function discovery(init: DiscoveryInit): (components: DiscoveryComponents) => DiscoveryClass {
+    return (components: DiscoveryComponents) => new DiscoveryClass(init, components)
 }
 
 class DiscoveryClass extends TypedEventEmitter<PeerDiscoveryEvents> implements PeerDiscovery, PeerDiscoveryProvider, Startable {
+    
     private discovery: any
-    private readonly init: DiscoveryInit & { peerId: string }
+    private readonly init: DiscoveryInit & { peerId: string, userAgent: string }
     private readonly components: DiscoveryComponents
     private readonly log: Logger
+    private readonly queue: string[] = []
     
     constructor(init: DiscoveryInit, components: DiscoveryComponents){
         super()
+        
         this.components = components
         this.init = Object.assign({
-            peerId: this.components.peerId.toString()
+            peerId: this.components.peerId.toString(),
+            userAgent: USER_AGENT
         }, init)
         this.log = components.logger.forComponent('libp2p:discovery')
     }
-    get [peerDiscoverySymbol]() {
-        return this
-    }
+
+    readonly [peerDiscoverySymbol] = this
+    readonly [Symbol.toStringTag] = 'jinx/discovery'
+    readonly [serviceCapabilities]: string[] = [
+      '@libp2p/peer-discovery'
+    ]
+
     start() {
         if(this.discovery) return
         this.discovery = new Discovery(this.init)
-        this.discovery.on('peer', (peer: string /*| { id: string, host: string, port: string }*/, source: 'tracker'|'dht'|'lsd') => {
-            let [host, port] = peer.split(':')
-            let peerInfo: PeerInfo = {
-                id: peerIdFromMultihash(identity.digest(text2arr(peer))),
-                multiaddrs: [ multiaddr(`/ip4/${host}/tcp/${port}`) ]
-            }
-            this.safeDispatchEvent('peer', { detail: peerInfo })
-        })
-        this.discovery.on('warning', (err: Error) => {
-            this.log.error('warning', err)
-        })
-        this.discovery.on('error', (err: Error) => {
-            this.log.error('error', err)
-        })
+        this.discovery.addEventListener('peer', this.onPeer)
+        this.discovery.addEventListener('warning', this.onWarning)
+        this.discovery.addEventListener('error', this.onError)
+        this.components.events.addEventListener('peer:disconnect', this.onDisconnect)
     }
+
+    private onPeer = async (ipport: string /*| { id: string, ip: string, port: string }*/, source: 'tracker'|'dht'|'lsd') => {
+        this.queue.push(ipport)
+        this.drain()
+    }
+    private onWarning = (err: Error) => {
+        this.log.error('warning', err)
+    }
+    private onError = (err: Error) => {
+        this.log.error('error', err)
+    }
+    private onDisconnect = (event: CustomEvent<PeerId>) => {
+        this.drain()
+    }
+
     stop() {
+        this.components.events.removeEventListener('peer:disconnect', this.onDisconnect)
         return new Promise<void>((res) => {
             this.discovery.destroy(() => {
                 this.discovery = undefined
@@ -65,9 +91,18 @@ class DiscoveryClass extends TypedEventEmitter<PeerDiscoveryEvents> implements P
             })
         })
     }
-}
 
-export function discovery(init: DiscoveryInit): (components: DiscoveryComponents) => DiscoveryClass {
-    return (components: DiscoveryComponents) => new DiscoveryClass(init, components)
+    private drain(){
+        let cm = this.components.connectionManager
+        while(this.queue.length && cm.getConnections().length >= cm.getMaxConnections()){
+            const ipport = this.queue.shift()
+            let [ip, port]: [string, number] = addrToIPPort(ipport)
+            let peerAddr = ipPortToMultiaddr(ip, port)
+            //let peerId = peerIdFromMultihash(identity.digest(text2arr(peer)))
+            cm.openConnection(peerAddr)
+            //.then(connection => this.safeDispatchEvent('peer', { detail: connection.remotePeer }))
+            .catch(err => this.log.error('could not dial discovered peer', err)) //TODO: Log addr
+        }
+    }
 }
   
--- index.ts
+++ index.ts
@@ -5,25 +5,56 @@ import { tcp } from '@libp2p/tcp'
 import { createLibp2p } from 'libp2p'
 import { discovery } from './discovery'
 import { hash } from 'uint8-util'
+import { promises as fs } from 'fs'
 
+const port = 5118
 const node = await createLibp2p({
     addresses: {
-        listen: [ `/ip4/0.0.0.0/tcp/${5118}` ]
+        listen: [ `/ip4/0.0.0.0/tcp/${port}` ]
     },
     transports: [ tcp() ],
     streamMuxers: [ yamux() ],
     connectionEncrypters: [ noise() ],
-    peerDiscovery: [ discovery({
-        infoHash: (await hash('', 'hex', 'sha-1')) as string,
-        port: 5118,
-        announce: [],
-        dht: true,
-        dhtPort: 5118,
-        userAgent: '',
-        tracker: true,
-        lsd: true,
-    }) ],
+    //peerDiscovery: [],
     services: {
+        discovery: discovery({
+            infoHash: (await hash(`jinx/launcher/${0}`, 'hex', 'sha-1')) as string,
+            port: port,
+            announce: await getAnnounceAddrs(),
+            dht: true,
+            dhtPort: 5117,
+            tracker: true,
+            lsd: true,
+        }),
         pubsub: gossipsub()
     }
 })
+
+const trackerListsURLS = [
+    'https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt',
+    'https://ngosang.github.io/trackerslist/trackers_best.txt',
+    'https://cdn.jsdelivr.net/gh/ngosang/trackerslist@master/trackers_best.txt',
+]
+
+async function getAnnounceAddrs(){
+    let list: string
+    try {
+        list = await fs.readFile('trackers.txt', 'utf-8')
+    } catch(e) {
+        console.log(e)
+        for(let url of trackerListsURLS){
+            try {
+                list = await (await fetch(url)).text()
+                try {
+                    /*await*/ fs.writeFile('trackers.txt', list, 'utf-8')
+                } catch(e) {
+                    console.log(e)
+                }
+            } catch(e) {
+                console.log(e)
+                continue
+            }
+        }
+    }
+    return (list ||= '').split('\n').filter(l => !!l)
+}
