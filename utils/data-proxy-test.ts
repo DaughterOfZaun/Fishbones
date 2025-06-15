4a8c6b6167e74bd4c3cff0fd13360ae909742c8e
DaughterOfZaun <jinx52538@gmail.com>
2025-06-15 18:12:07 +0300

    wip

--- utils/data-proxy-test.ts
+++ utils/data-proxy-test.ts
@@ -4,19 +4,20 @@ import { tcp } from "@libp2p/tcp"
 import { webRTC, webRTCDirect } from "@libp2p/webrtc"
 import { createLibp2p } from "libp2p"
 import { LOCALHOST } from "./constants"
+import { ProxyClient, ProxyServer } from "./data-proxy"
 
 const createNode = async () => {
     const node = await createLibp2p({
         addresses: {
             listen: [
                 `/ip4/0.0.0.0/tcp/${0}`,
-                `/ip4/0.0.0.0/udp/${0}/webrtc`,
+                //`/ip4/0.0.0.0/udp/${0}/webrtc`,
                 `/ip4/0.0.0.0/udp/${0}/webrtc-direct`,
             ]
         },
         transports: [
             webRTCDirect(),
-            webRTC(),
+            //webRTC(),
             tcp(),
         ],
         streamMuxers: [ yamux() ],
@@ -26,13 +27,56 @@ const createNode = async () => {
     return node
   }
 
-async function testServer(){
-    const gss = await Bun.udpSocket({ binaryType: 'uint8array', hostname: LOCALHOST })
-    console.log('game server listens on', gss.port)
+await test()
+async function test(){
+    const gameServerSocket = await Bun.udpSocket({
+        binaryType: 'uint8array',
+        hostname: LOCALHOST,
+        socket: {
+            data(socket, data, port, address) {
+                console.log('game server socket recieved', data, 'from', port, address)
+            },
+        },
+    })
+    console.log('game server listens on', gameServerSocket.port)
 
-    const gsc = await Bun.udpSocket({ binaryType: 'uint8array', hostname: LOCALHOST })
-    console.log('game client listens on', gsc.port)
+    const gameClientSocket = await Bun.udpSocket({
+        binaryType: 'uint8array',
+        hostname: LOCALHOST,
+        socket: {
+            data(socket, data, port, address) {
+                console.log('game client socket recieved', data, 'from', port, address)
+            },
+        },
+    })
+    console.log('game client listens on', gameClientSocket.port)
 
-    const gsn = await createNode()
-    const gcn = await createNode()
+    const serverNode = await createNode()
+    console.log('lobby server node addrs', serverNode.getMultiaddrs().map(ma => ma.toString()))
+
+    const clientNode = await createNode()
+    console.log('lobby client node addrs', clientNode.getMultiaddrs().map(ma => ma.toString()))
+
+    await serverNode.peerStore.patch(clientNode.peerId, { multiaddrs: clientNode.getMultiaddrs() })
+    await clientNode.peerStore.patch(serverNode.peerId, { multiaddrs: serverNode.getMultiaddrs() })
+
+    const proxyServer = new ProxyServer(serverNode)
+    const proxyClient = new ProxyClient(clientNode)
+
+    await proxyServer.start(gameServerSocket.port, [ clientNode.peerId ])
+    await proxyClient.connect(serverNode.peerId)
+
+    const serverOnClientPort = proxyClient.getPort(serverNode.peerId)!
+    gameClientSocket.send(new Uint8Array([1, 2, 3, 4]), serverOnClientPort, LOCALHOST)
+
+    //await new Promise(res => setTimeout(res, 100))
+
+    const clientOnServerPort = proxyServer.getPort(clientNode.peerId)!
+    gameServerSocket.send(new Uint8Array([1, 2, 3, 4]), clientOnServerPort, LOCALHOST)
+
+    gameServerSocket.close()
+    gameClientSocket.close()
+
+    serverNode.stop()
+    clientNode.stop()
 }
--- utils/data-proxy.ts
+++ utils/data-proxy.ts
@@ -7,7 +7,6 @@ import * as lp from 'it-length-prefixed'
 import { EventIterator } from 'event-iterator'
 import type { Duplex, Source } from 'it-stream-types'
 import type { Uint8ArrayList } from 'uint8arraylist'
-import type { Multiaddr } from "@multiformats/multiaddr";
 
 const log = logger('launcher:proxy')
 
@@ -74,27 +73,39 @@ class Proxy {
     protected readonly peers = new Map<PeerIdStr, PeerData>()
     protected readonly node: Libp2p
 
-    protected constructor(node: Libp2p){
+    public constructor(node: Libp2p){
         this.node = node
     }
 
-    protected async handleStream(id: PeerId, stream: Stream, remoteAddress?: SocketAddress){
+    public getPort(id: PeerId){
+        return this.peers.get(id.toString())?.socket.port
+    }
+
+    protected async createPeer(id: PeerId, stream?: Stream, remoteAddress?: SocketAddress){
         const idStr = id.toString()
 
+        const socket = await createSocket(remoteAddress)
+        const peer = { socket, stream }
+        this.peers.set(idStr, peer)
+        return peer
+    }
+
+    protected /*async*/ handleStream(id: PeerId, stream: Stream, /*remoteAddress?: SocketAddress*/){
+        const idStr = id.toString()
+
+        // eslint-disable-next-line prefer-const
         let peer = this.peers.get(idStr)
         if(!peer){
-            const socket = await createSocket(remoteAddress)
-            peer = { socket, stream }
-            this.peers.set(idStr, peer)
+            return //peer = await createPeer(id, remoteAddress)
         } else {
             // Only one stream per connection is allowed.
-            peer.stream.close().catch(err => log.error(err))
+            peer.stream?.close().catch(err => log.error(err))
             peer.stream = stream
             //TODO: close pipes
         }
 
         pipe(
-            peer.stream.source,
+            peer.stream!.source,
             (source) => lp.decode(source),
             peer.socket.stream.sink,
         ).catch(err => log.error(err))
@@ -102,13 +113,13 @@ class Proxy {
         pipe(
             peer.socket.stream.source,
             (source) => lp.encode(source),
-            peer.stream.sink,
+            peer.stream!.sink,
         ).catch(err => log.error(err))
     }
 
     protected disconnect(){
         for(const peer of this.peers.values()){
-            peer.stream.close().catch(err => log.error(err))
+            peer.stream?.close().catch(err => log.error(err))
             //TODO: peer.socket.stream.close()
             peer.socket.close()
             //TODO: close pipes
@@ -119,20 +130,25 @@ class Proxy {
 
 
 type PeerIdStr = string
-type PeerData = { socket: CustomizedSocket, stream: Stream }
+type PeerData = { socket: CustomizedSocket, stream?: Stream }
 export class ProxyServer extends Proxy {
 
     private readonly peerIds = new Map<PeerIdStr, PeerId>()
-    private readonly localAddress: SocketAddress
-
-    constructor(node: Libp2p, peerIds: PeerId[], port: number) {
-        super(node)
-        for(const id of peerIds)
-            this.peerIds.set(id.toString(), id)
-        this.localAddress = { port, address: LOCALHOST }
-    }
+    private readonly localAddress: SocketAddress = { address: LOCALHOST, port: 0, }
     
-    public start(){
+    public async start(port: number, peerIds: PeerId[]){
+
+        this.localAddress.port = port
+        for(const id of peerIds){
+            this.peerIds.set(id.toString(), id)
+        }
+
+        await Promise.all(
+            peerIds.map(id =>
+                this.createPeer(id, undefined, this.localAddress)
+            )
+        )
+
         this.node.handle(PROXY_PROTOCOL, this.protocolHandler, {
             maxInboundStreams: 1,
             maxOutboundStreams: 1,
@@ -146,8 +162,9 @@ export class ProxyServer extends Proxy {
 
     private protocolHandler({ stream, connection }: IncomingStreamData){
         const id = connection.remotePeer
-        if(this.peerIds.has(id.toString())){
-            /*await*/ this.handleStream(id, stream, this.localAddress)
+        //if(this.peerIds.has(id.toString())){
+        if(this.peers.has(id.toString())){
+            /*await*/ this.handleStream(id, stream, /*this.localAddress*/)
         } else {
             stream.close().catch(err => log.error(err))
             return
@@ -156,14 +173,22 @@ export class ProxyServer extends Proxy {
 }
 
 export class ProxyClient extends Proxy {
-    constructor(node: Libp2p){
-        super(node)
-    }
-    public async connect(id: PeerId, addr: Multiaddr){
-        const stream = await this.node.dialProtocol(addr, PROXY_PROTOCOL, { maxOutboundStreams: 1 })
+    private serverId?: PeerId
+    public async connect(id: PeerId){
+        this.serverId = id
+        const peer = await this.node.peerStore.getInfo(id)
+        const addrs = peer.multiaddrs.filter(ma => ma.toOptions().transport == 'udp')
+        const opts = { force: false, maxOutboundStreams: 1 } //as DialProtocolOptions & { force: boolean }
+        const stream = await this.node.dialProtocol(addrs, PROXY_PROTOCOL, opts)
+        const peer = await this.createPeer(id, stream)
         this.handleStream(id, stream)
     }
     public disconnect(){
+        this.serverId = undefined
         super.disconnect()
+    }
+    public getPort(id = this.serverId){
+        console.assert(!id || id.equals(this.serverId))
+        return id && super.getPort(id)
     }
 }
