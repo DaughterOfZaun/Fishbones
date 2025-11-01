e058d8c1a7a4f7f176a06b67a6b3bae0555a7798
DaughterOfZaun <jinx52538@gmail.com>
2025-11-01 22:17:07 +0300

    move files

--- index-failsafe.ts
+++
@@ -1,6 +0,0 @@
-import { launchTerminal } from "./utils/data-terminal";
-if(/*await*/ launchTerminal() === false) try {
-    await import('./index.ts')
-} catch(e) {
-    console.error('Main module import failed', e)
-}
